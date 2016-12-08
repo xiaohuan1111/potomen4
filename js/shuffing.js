291164476d9b9a82316a66da3de3e10e96f56079
ouyangxinaxin <1143203941@qq.com>
2016-12-08 14:43:59 +0800

    底部完成

--- js/shuffing.js
+++ js/shuffing.js
@@ -1,43 +1,4 @@
 $(document).ready(function () {
-    var nowimg = 0;
-    var lis = $(".slides li").length;
-    var $slides = $(".slides");
-    var $div = $(".footer-right-widget");
-    var timer = null;
-    $(".slides li:first").clone().appendTo(".slides");
-    console.log(lis)
-    $(".footer-right-next").click(rightFunc)
-    function rightFunc(event){
-        if (nowimg < lis){
-            nowimg++;
-            $slides.animate({"left":nowimg*-336},100);
-        }else {
-            nowimg = 0;
-            $slides.animate({"left":lis*-336}, 100, "linear", function () {
-                $slides.css("left", 0);
-                // $(".slides li:first").clone().appendTo(".slides");
-            })
-        }
-    }
-    $(".footer-right-prev").click(function (event) {
-        if(nowimg > 0){
-            nowimg--;
-            $slides.animate({"left":nowimg*-336},100);
-        }else {
-            nowimg = lis;
-            $slides.css({"left":lis*-336},100);
-            $slides.animate({"left":nowimg*-336},100);
-        }
-    });
-
-    timer = setInterval(rightFunc, 2000);
-    $div.mouseenter(function (event) {
-        clearInterval(timer)
-    });
-    $div.mouseleave(function (event) {
-        clearInterval(timer);
-        timer = setInterval(rightFunc, 2000);
-    });
 
     //返回顶部
     $(window).scroll(function(){
